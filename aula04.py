7d01ffe06458711fa4a5cd3c2b118ec783e10785
estrela1921 <143469066+estrela1921@users.noreply.github.com>
2023-09-26 14:44:42 -0300

    Add files via upload

---
+++ aula04.py
@@ -0,0 +1,48 @@
+
+
+#Escreva um loop que inicia no último caractere da string e caminha para o
+#primeiro caractere, imprimindo cada letra em uma linha separada;
+#2. Dado que a variável sentenca é uma string, qual o resultado de sentenca[:]?
+#3. Teste alterar o valor de um caractere de uma string.
+
+#string="muito dificil"
+#ultimocaracter=len(string)
+
+#for i in range(ultimocaracter):
+    #ultimocaracter -=1
+
+    #print(string[ultimocaracter])
+#sentença="muito dificil" 
+#nova_sentença="demais"+
+#print("nova_sentença")
+
+#Use a função find e o fatiamento de strings para extrair a porção da string
+#depois do sinal de dois pontos, converta a string extraída em um número e
+#multiplique por 100. Exiba o resultado em tela.
+str = 'X-DSPAM-Confidence:0.8475'
+print(len(str))
+print(str.find(":"))
+print(str)
+print(str[19:25])
+numero=float("19:25")
+
+
+#mensagem = '0.8475'
+#print(mensagem.find(:'0.8475')) *100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
